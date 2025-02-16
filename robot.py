36aa62080edf7156bf215092b0aaf39dbc76e7a2
DBinK <DBinKv1@Gmail.com>
2025-02-16 23:03:00 +0800

    feat: 添加相机模块和apriltag检测功能
    - 新增camera.py文件，实现USB相机初始化和图像捕捉
    - 新增vision.py文件，实现apriltag检测和图像绘制
    - 更新motion.py和robot.py，增加软复位功能和优化运动控制
    - 添加main.py文件，作为项目入口

--- robot.py
+++ robot.py
@@ -4,44 +4,47 @@ from motion import BambuMotion
 class BambuRobot(BambuMotion):
     def __init__(self, reset=True):
         super().__init__(reset=reset)
-        self.STANDBY_Z = 30  # 待机高度
-        self.CHESS_Z = 10    # 棋子高度
-        self.Z_SPEED = 48000
+        self.STANDBY_Z = 30   # 待机高度
+        self.CHESS_Z = 10     # 棋子高度
+        self.Z_SPEED = 48000  # Z轴速度
 
-    def move_z(self, z, speed=None):
+    def move_z(self, pz, speed=None):
         if speed is None:
             speed = self.Z_SPEED
-        super().move_z(z, speed)
+        super().move_z(pz, speed)
 
     def capture_piece(self, from_x, from_y):
+        print("开始 捕获棋子")
         self.move(from_x, from_y, self.STANDBY_Z)  # 移动到棋子 起始位置
         self.move_z(self.CHESS_Z)                  # 下降 
-        time.sleep(1)  # 估计时间, 函数是非阻塞的, 需要手动暂停等待
 
+        print("用电磁铁 拾取棋子")
+        self.notice_finish()  
         pass                                       # 用电磁铁 拾取棋子
-        time.sleep(1)                              # 等待吸起棋子
+        #time.sleep(2)                              # 等待吸起棋子
 
         self.move_z(self.STANDBY_Z)                # 抬起 
-        time.sleep(1)  # 手动暂停等待
 
         print(f"捕获棋子于 ({from_x}, {from_y}) \n")
 
     def release_piece(self, to_x, to_y):
+        print("开始 释放棋子")
         self.move(to_x, to_y, self.STANDBY_Z)  # 移动到棋子 目标位置
         self.move_z(self.CHESS_Z)              # 下降 
-        time.sleep(1)  # 估计时间, 函数是非阻塞的, 需要手动暂停等待
 
+        print("用电磁铁 释放棋子")
+        self.notice_finish()
         pass                                   # 用电磁铁 释放棋子
-        time.sleep(1)                          # 等待放下棋子
+        #time.sleep(2)                          # 等待放下棋子
 
         self.move_z(self.STANDBY_Z)            # 抬起
-        time.sleep(1)  # 手动暂停等待 
 
-        print(f"释放棋子于 ({to_x}, {to_y}) \n")
+        print(f"释放棋子到 ({to_x}, {to_y}) \n")
 
     def show_chess_board(self):
-        self.move_z(self.STANDBY_Z)          # 抬起
-        self.move(273, 240, self.STANDBY_Z, 20000)  # 移动到待机处
+        print("展示棋盘")
+        self.move_z(self.STANDBY_Z)                 # 抬起
+        self.move(273, 250, self.STANDBY_Z, 30000)  # 移动到待机处
         print("待机并展示棋盘 \n")
 
     def move_piece(self, from_x, from_y, to_x, to_y):
@@ -57,4 +60,7 @@ if __name__ == "__main__":
     robot = BambuRobot(reset=False)
     # robot = BambuRobot(reset=True)
 
-    robot.move_piece(150, 150, 50, 50)
+    robot.move_piece(180, 180, 50, 50)
+
+    time.sleep(10)
+    robot.soft_reset(30000)
---
+++ vision.py
@@ -0,0 +1,78 @@
+from pupil_apriltags import Detector
+import cv2
+
+
+# 初始化一个Detector实例，用于检测和解码Apriltag标记
+at_detector = Detector(
+    families="tag16h5",  # 指定使用的Apriltag家族，这里选择的是'tag16h5'
+    nthreads=1,  # 设置用于加速计算的线程数，此处设置为1
+    quad_decimate=1.0,  # 设置图像简化比例，用于加快处理速度，1.0表示不简化
+    quad_sigma=0.0,  # 指定在检测标记前对图像进行高斯模糊的程度，0.0表示不进行模糊处理
+    refine_edges=1,  # 设置是否对检测到的标记边缘进行精细化处理，以提高定位精度
+    decode_sharpening=0.25,  # 设置解码过程中的图像锐化程度，以提高解码成功率
+    debug=0,  # 设置调试模式级别，0表示不启用调试模式
+)
+
+
+def detect(img):
+    # 预处理
+    img_gary = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 将图像转换为灰度图像
+    # img = cv2.GaussianBlur(img, (7, 7), 0)  # 应用高斯滤波以平滑图像
+    img_bin = cv2.threshold(img_gary, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]  # 二值化
+
+    # 检测标记
+    detections = at_detector.detect(img_bin)
+
+    for detection in detections:
+        print(detection.tag_id)
+
+    return detections
+
+
+def draw(img, detections):
+
+    img_draw = img.copy()
+
+    # 绘制检测结果
+    for detection in detections:
+        corners = detection.corners
+
+        # 绘制边界框
+        for i in range(4):
+            cv2.line(
+                img_draw,
+                tuple(corners[i].astype(int)),
+                tuple(corners[(i + 1) % 4].astype(int)),
+                (0, 255, 0),
+                2,
+            )  # 绿色线条
+
+        # 在中心绘制标签 ID
+        center = int(detection.center[0]), int(detection.center[1])
+        cv2.putText(
+            img_draw,
+            f"ID: {detection.tag_id}",
+            center,
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 0, 0),
+            2,
+        )  # 红色文本
+
+    return img_draw
+
+
+if __name__ == "__main__":
+    # 读取图像
+    img = cv2.imread("test/apriltag.jpg")
+
+    # 检测标记
+    detections = detect(img)
+
+    # 绘制检测结果
+    img_draw = draw(img, detections)
+
+    # 显示结果
+    cv2.imshow("Detected AprilTags", img_draw)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
